8643f1af91e51e1b79416c2a8ad4e419a97fec42
Amirhosein <amirhoseinrad25@gmail.com>
2023-03-24 15:43:14 +0330

    feat: Added tooltip

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -6,9 +6,18 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import { InlineSVGModule } from 'ng-inline-svg-2';
 import { DragDropModule } from 'primeng/dragdrop';
+import { TooltipModule } from 'primeng/tooltip';
 
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [CommonModule, DashboardRoutingModule, LeafletModule, CarouselModule, InlineSVGModule, DragDropModule],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule,
+    LeafletModule,
+    CarouselModule,
+    InlineSVGModule,
+    DragDropModule,
+    TooltipModule,
+  ],
 })
 export class DashboardModule {}
