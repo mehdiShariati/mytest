27eaa13dd3546a7bf3ff5d79bf0ab57974d83d82
Amirhosein <amirhoseinrad25@gmail.com>
2023-03-27 11:35:54 +0330

    docs: Improved style and structure

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -79,7 +79,7 @@ export class DashboardComponent {
 
   slides = [
     { id: 1, photo: './assets/images/Space.png' },
-    { id: 2, photo: './assets/images/2.png' },
+    { id: 2, photo: './assets/images/pic2.jpg' },
     { id: 3, photo: './assets/images/3.png' },
   ];
   stories = [
@@ -96,6 +96,7 @@ export class DashboardComponent {
 
   apps = [
     { color: '#FF8389', id: 2, subject: 'خطای سامانه', icon: 'assets/icons/system-error.svg', location: null },
+    { color: '#33B1FF', id: 12, subject: 'نیروی انسانی', icon: 'assets/icons/manpower.svg', location: null },
     { color: '#FF7EB6', id: 6, subject: 'پرسشنامه', icon: 'assets/icons/questionnaire.svg', location: null },
     { color: '#42BE65', id: 7, subject: 'اشراف', icon: 'assets/icons/nobles.svg', location: null },
     { color: '#FF8389', id: 8, subject: 'مدیریت پروژه', icon: 'assets/icons/project-management.svg', location: null },
